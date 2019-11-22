ce352aa21405df9ae204af3e184a02067957f2bb
John Cyrill Corsanes <johncyril@gorated.ph>
2019-11-22 21:57:23 +0800

    refactor(all): huge Refactor based on gulpfile.js
    BREAKING CHANGE: Gulp Errors

--- config.rb
+++ config.rb
@@ -4,9 +4,9 @@ require 'compass/import-once/activate'
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
-sass_dir = "sass"
+sass_dir = "src/sass"
 images_dir = "images"
-javascripts_dir = "javascripts"
+javascripts_dir = "/src/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
