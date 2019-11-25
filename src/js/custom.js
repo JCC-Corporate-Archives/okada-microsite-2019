9972960228340617ba49123a8a646ba76d0eaaed
John Cyrill Corsanes <johncyril@gorated.ph>
2019-11-25 16:27:32 +0800

    improvement(homepage, hero-section): hero section styling

---
+++ src/js/custom.js
@@ -0,0 +1,11 @@
+$('#collapsibleNavbar').on('show.bs.collapse', function () {
+  $('#navBar').css({
+    'background': '#000'
+  });
+});
+
+$('#collapsibleNavbar').on('hide.bs.collapse', function () {
+  $('#navBar').css({
+    'background': 'transparent'
+  });
+});
