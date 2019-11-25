bf8a9ff05b66456e13eb595dc3240dd904b6c8d9
John Cyrill Corsanes <johncyril@gorated.ph>
2019-11-25 16:34:58 +0800

    test(pipeline): custom.js doublequote & removed compass-style

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,11 +1,11 @@
-$('#collapsibleNavbar').on('show.bs.collapse', function () {
-  $('#navBar').css({
-    'background': '#000'
+$("#collapsibleNavbar").on("show.bs.collapse", function () {
+  $("#navBar").css({
+    "background": "#000"
   });
 });
 
-$('#collapsibleNavbar').on('hide.bs.collapse', function () {
-  $('#navBar').css({
-    'background': 'transparent'
+$("#collapsibleNavbar").on("hide.bs.collapse", function () {
+  $("#navBar").css({
+    "background": "transparent"
   });
 });
